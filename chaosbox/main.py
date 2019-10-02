7ff7d19316c017534dc64eb6a2644edb71df25fa
fabio-litscher <5813970+fabio-litscher@users.noreply.github.com>
2019-10-02 10:39:24 +0200

    added template files
    added template files and routes for the different pages.

--- chaosbox/main.py
+++ chaosbox/main.py
@@ -7,9 +7,23 @@ app = Flask("chaosbox")
 def home():
 	return render_template('index.html', name="Fabio")
 
-@app.route('/detail')
-def details():
-	return render_template('detail.html', box_id="123456789")
+@app.route('/detail/box')
+def boxDetails():
+	return render_template('boxDetail.html', box_id="123456789")
+
+@app.route('/detail/item')
+def itemDetails():
+	return render_template('itemDetail.html', box_id="123456789")
+
+@app.route('/create/box')
+def createBox():
+	return render_template('createBox.html')
+
+@app.route('/create/item')
+def createItem():
+	return render_template('createItem.html')
+
+
 
 '''@app.route("/test")
 def test():
