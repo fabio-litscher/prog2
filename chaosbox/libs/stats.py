503d0814e3a9ef42c5d10672c99eb707b47082b4
fabio-litscher <5813970+fabio-litscher@users.noreply.github.com>
2019-12-16 16:12:49 +0100

    update statistics

--- chaosbox/libs/stats.py
+++ chaosbox/libs/stats.py
@@ -1,15 +1,18 @@
 def calc_stats(boxes):
     count_boxes = len(boxes)
     count_items = 0
+    max_items = 0
 
     for box in boxes:
+        if len(boxes[box]['box_items']) > max_items: max_items = len(boxes[box]['box_items'])
         for item in boxes[box]['box_items']:
             count_items = count_items + 1
 
     stats = {
         'Anzahl Boxen': count_boxes,
         'Anzahl Items': count_items,
-        'Durchsch. Items pro Box': round(count_items/count_boxes, 2)
+        'Durchschnittliche Items pro Box': round(count_items/count_boxes, 2),
+        'Meiste Items in Box': max_items
     }
 
     return stats
