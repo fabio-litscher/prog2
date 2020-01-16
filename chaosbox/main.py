e21ecc5d8285292816103aecb4b38851a00e7f59
fabio-litscher <5813970+fabio-litscher@users.noreply.github.com>
2020-01-16 14:39:57 +0100

    some small refactoring

--- chaosbox/main.py
+++ chaosbox/main.py
@@ -9,14 +9,14 @@ from flask import url_for
 import time
 from libs import data
 from libs import stats
+from libs import boxes as box_lib
 
 app = Flask("chaosbox")
 app_main_path = Path(os.path.abspath(
     "/".join(os.path.realpath(__file__).split("/")[:-1])))
 data_path = Path(os.path.abspath(app_main_path / "data"))
-# app.secret_key = 'the random string'
 data_storage_file = data_path / "chaosbox_boxes.json"
-stats_storage_file = data_path / "chaosbox_stats.json"
+#stats_storage_file = data_path / "chaosbox_stats.json"
 
 
 # initialize boxes
@@ -37,33 +37,31 @@ def home():
 @app.route('/box/<box_id>', methods=['GET', 'POST'])
 @app.route('/box/<box_id>/<edit_box>', methods=['GET', 'POST'])
 def box(box_id=None, edit_box=None):
+    boxes = data.load_json(data_storage_file)
+
     if request.method == 'POST':
         box_name = request.form['box_name']
         box_description = request.form['box_description']
 
-        # when editbox set box_id ootherwise create new from timestamp in ms
+        # when editbox set box_id otherwise create new from timestamp in ms
         if 'box_id' in request.form:
             box_id = request.form['box_id']
-            boxes[box_id]['box_name'] = box_name
-            boxes[box_id]['box_description'] = box_description
+            boxes = box_lib.update_box(boxes, box_id, box_name, box_description)
         else:
             box_id = str(int(round(time.time() * 1000)))
-            boxes[box_id] = {
-                'box_name': box_name,
-                'box_description': box_description,
-                'box_items': {}
-            }
+            boxes = box_lib.add_new_box(boxes, box_id, box_name, box_description)
 
         data.save_json(data_storage_file, boxes)
 
         box = boxes[box_id]
         return render_template('box.html', box_id=box_id, box=box)
-
+    
+    # edit box
     if edit_box == "edit":
         try:
             box = boxes[box_id]
             return render_template('box.html', edit_box_id=box_id, box=box)
-        except:
+        except():
             return redirect(url_for('home'))
 
     # show details
@@ -71,7 +69,7 @@ def box(box_id=None, edit_box=None):
         try:
             box = boxes[box_id]
             return render_template('box.html', box_id=box_id, box=box)
-        except:
+        except():
             return redirect(url_for('home'))
 
     return render_template('box.html')
@@ -80,7 +78,7 @@ def box(box_id=None, edit_box=None):
 @app.route('/box/delete/<box_id>', methods=['GET', 'POST'])
 def delete_box(box_id=None):
     if request.method == 'POST':
-        boxes.pop(box_id, None)
+        boxes.pop(box_id)
 
         data.save_json(data_storage_file, boxes)
 
@@ -125,7 +123,6 @@ def item(box_id=None, item_id=None, edit_item=None):
                 'item_quantity': item_quantity
             }
 
-
         """item_name = request.form['item_name']
         item_description = request.form['item_description']
         item_quantity = request.form['item_quantity']"""
@@ -138,7 +135,7 @@ def item(box_id=None, item_id=None, edit_item=None):
         try:
             item = boxes[box_id]['box_items'][item_id]
             return render_template('item.html', box_id=box_id, edit_item_id=item_id, item=item)
-        except:
+        except():
             return redirect(url_for('home'))
 
     # show existing item
@@ -154,7 +151,7 @@ def item(box_id=None, item_id=None, edit_item=None):
 @app.route('/box/delete/<box_id>/<item_id>', methods=['GET', 'POST'])
 def delete_item(box_id=None, item_id=None):
     if request.method == 'POST':
-        boxes[box_id]['box_items'].pop(item_id, None)
+        boxes[box_id]['box_items'].pop(item_id)
 
         data.save_json(data_storage_file, boxes)
 
