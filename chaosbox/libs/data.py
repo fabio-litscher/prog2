5fd96defda9633d368070b8d3f3f7d72339097f5
fabio-litscher <5813970+fabio-litscher@users.noreply.github.com>
2020-01-17 09:02:39 +0100

    bugfix of stats when no boxes in dict

--- chaosbox/libs/data.py
+++ chaosbox/libs/data.py
@@ -1,7 +1,19 @@
+"""
+data.py is a lib which helps to handle all interactions with the json file (read/write)
+furthermore it has a function to create dummy data which was used at the beginning of the project
+"""
 import json
 
 
 def load_json(json_path):
+    """
+    Loads all the warehouse data from the json file
+
+    Args:
+        json_path (str): path to the json file
+    Returns:
+        dict: A dictionary containing the warehouse data. Returns an empty structure if file not found
+    """
     try:
         with open(json_path) as open_file:
             boxes = json.load(open_file)
@@ -12,11 +24,24 @@ def load_json(json_path):
 
 
 def save_json(json_path, data):
+    """
+    Writes all the warehouse data from into the json file
+
+    Args:
+        json_path (str): path to the json file
+        data (dict): dict with all the warehouse data to write into the file
+    """
     with open(json_path, "w+", encoding="utf-8") as open_file:
         json.dump(data, open_file, indent=4)
 
 
 def create_dummy_data():
+    """
+    Creates dict with dummy warehouse data including boxes and items
+
+    Returns:
+        dict: A dictionary with dummy warehouse data in the same structure as prod data would be
+    """
     boxes = {
         '0': {
             'box_name': 'Testbox 1',
--- chaosbox/libs/stats.py
+++ chaosbox/libs/stats.py
@@ -4,21 +4,30 @@ def calc_stats(boxes):
     max_items = 0
     max_items_box = ""
 
-    for box in boxes:
-        if len(boxes[box]['box_items']) > max_items:
-            max_items = len(boxes[box]['box_items'])
-            max_items_box = "<a href='./box/" + box + "'>" + boxes[box]['box_name'] + "</a> mit " + str(max_items) + " items"
-        elif len(boxes[box]['box_items']) == max_items:
-            max_items_box += ", <a href='./box/" + box + "'>" + boxes[box]['box_name'] + "</a> mit " + str(max_items) + " items"
+    if count_boxes > 0:
+        for box in boxes:
+            if len(boxes[box]['box_items']) > max_items:
+                max_items = len(boxes[box]['box_items'])
+                max_items_box = "<a href='./box/" + box + "'>" + boxes[box]['box_name'] + "</a> mit " + str(max_items) + " items"
+            elif len(boxes[box]['box_items']) == max_items:
+                max_items_box += ", <a href='./box/" + box + "'>" + boxes[box]['box_name'] + "</a> mit " + str(max_items) + " items"
 
-        for item in boxes[box]['box_items']:
-            count_items = count_items + 1
+            for item in boxes[box]['box_items']:
+                count_items = count_items + 1
 
-    stats = {
-        'Anzahl Boxen': count_boxes,
-        'Anzahl Items': count_items,
-        'Durchschnittliche Items pro Box': round(count_items / count_boxes, 2),
-        'Box(en) mit den meisten Items': max_items_box
-    }
+        stats = {
+            'Anzahl Boxen': count_boxes,
+            'Anzahl Items': count_items,
+            'Durchschnittliche Items pro Box': round(count_items / count_boxes, 2),
+            'Box(en) mit den meisten Items': max_items_box
+        }
+
+    else:
+        stats = {
+            'Anzahl Boxen': "-",
+            'Anzahl Items': "-",
+            'Durchschnittliche Items pro Box': "-",
+            'Box(en) mit den meisten Items': "-"
+        }
 
     return stats
